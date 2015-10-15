565fe77080b30b86f6123c762a4315c4c30291ea
Rafael Blanco <rafa_de_cadiz@hotmail.com>
2015-10-15 21:44:48 +0200

    tarea jaime

--- mayor_cho.py
+++ mayor_cho.py
@@ -9,7 +9,10 @@ while num >=0:
 		
 	num = int(raw_input('mete un numero positivo (si te aburres mete un negativo y terminas): '))	
 
-print "El mayor del chorro números es " + str(mayor)
+if mayor > 0:
+	print "El mayor del chorro números es " + str(mayor)
 		
+if mayor <= 0:
+	print "Mete algun numero positivo primero si no no tiene gracia"		
 
 
