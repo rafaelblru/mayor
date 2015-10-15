c23c7dabac26dfdb7c59d132dd1bae256b5e646d
Rafael Blanco <rafa_de_cadiz@hotmail.com>
2015-10-15 21:12:34 +0200

    todavia aburrido

--- mayor_cho.py
+++ mayor_cho.py
@@ -6,7 +6,7 @@ while not aburrido:
 	num = int(raw_input('mete un numero positivo (si te aburres mete un negativo y terminas): '))
 	if num < 0:
 		aburrido = True
-	if num > mayor:
+	elif num > mayor:
 		mayor = num
 
 print "El mayor del chorro números es " + str(mayor)
