1a73103a6be4f8b041e8c42c1e2aff982dad12bd
Rafael Blanco <rafa_de_cadiz@hotmail.com>
2015-10-15 21:37:43 +0200

    version un poco mejorada

--- mayor_cho.py
+++ mayor_cho.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 mayor = 0
-entretenio= True
-while entretenio:
-	num = int(raw_input('mete un numero positivo (si te aburres mete un negativo y terminas): '))
-	if num < 0:
-		entretenio = False
-	elif num > mayor:
+num = int(raw_input('mete un numero positivo (si te aburres mete un negativo y terminas): '))
+while num >=0:
+	
+	if num > mayor:
 		mayor = num
+		
+	num = int(raw_input('mete un numero positivo (si te aburres mete un negativo y terminas): '))	
 
 print "El mayor del chorro números es " + str(mayor)
 		
