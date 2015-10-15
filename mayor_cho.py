c11d6d9361d4e725084579926c120cc60d91cbcc
Rafael Blanco <rafa_de_cadiz@hotmail.com>
2015-10-15 21:28:52 +0200

    mu entretenio

--- mayor_cho.py
+++ mayor_cho.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 mayor = 0
-aburrido = False
-while not aburrido:
+entretenio= True
+while entretenio:
 	num = int(raw_input('mete un numero positivo (si te aburres mete un negativo y terminas): '))
 	if num < 0:
-		aburrido = True
+		entretenio = False
 	elif num > mayor:
 		mayor = num
 
